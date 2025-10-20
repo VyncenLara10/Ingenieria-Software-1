4a052522d833838160e24ab8528fdc3facfe24ac
Roger Monterroso <124108851+Roger2808@users.noreply.github.com>
2025-10-20 13:46:39 -0600

    ya tiene el tilemapping y el mapa nuevo feo funcional, falta movilidad

--- src/main/java/main/Juego.java
+++ src/main/java/main/Juego.java
@@ -1,10 +1,13 @@
 package main;
 
 import gamestates.Gamestate;
+import static gamestates.Gamestate.MENU;
+import static gamestates.Gamestate.OPTIONS;
+import static gamestates.Gamestate.PLAYING;
+import static gamestates.Gamestate.QUIT;
 import gamestates.Menu;
 import gamestates.Playing;
 import java.awt.Graphics;
-
 
 
 public class Juego implements Runnable{
@@ -24,41 +27,29 @@ public class Juego implements Runnable{
     public final static int TILES_SIZE = (int)(TILES_DEFAULT_SIZE * SCALE);
     public final static int GAME_WIDTH = TILES_SIZE * TILES_IN_WIDTH;
     public final static int GAME_HEIGHT = TILES_SIZE * TILES_IN_HEIGHT;
-
+    
     public Juego(){
         initClasses();
-
-        try {
-            if (twoPlayers) {
-                if (isServer) {
-                    network = new GameServer(5040);
-                } else {
-                    network = new GameClient("localhost", 5040);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        
         paneljuego = new PanelJuego(this);
         ventanajuego = new VentanaJuego(paneljuego);
         paneljuego.setFocusable(true);
         paneljuego.requestFocus();
-
+        
         iniciarGameLoop();
     }
-
+    
     private void initClasses() {
         menu = new Menu(this);
         playing = new Playing(this);
 
     }
-
+    
     private void iniciarGameLoop(){
         hiloJuego = new Thread(this);
         hiloJuego.start();
     }
-
+    
     public void update(){
         switch(Gamestate.state){
             case MENU:
@@ -74,7 +65,7 @@ public class Juego implements Runnable{
                 break;
         }
     }
-
+    
     public void render(Graphics g){
         switch(Gamestate.state){
             case MENU:
@@ -86,6 +77,7 @@ public class Juego implements Runnable{
             default:
                 break;
         }
+        
     }
 
     @Override
@@ -93,42 +85,43 @@ public class Juego implements Runnable{
         double TiempoPorFrame = 1000000000.0 / FPS_SET;
         double TiempoPorUpdate = 1000000000.0 / UPS_SET;
         long tiempoAnterior = System.nanoTime();
-
+        
         int frames = 0;
         int updates = 0;
         long ultimoCheck = System.currentTimeMillis();
-
+        
         double deltaU = 0;
         double deltaF = 0;
-
+        
         while(true){
             long currentTime = System.nanoTime();
-
+            
             deltaU += (currentTime - tiempoAnterior) / TiempoPorUpdate;
             deltaF += (currentTime - tiempoAnterior) / TiempoPorFrame;
             tiempoAnterior = currentTime;
-
+            
             if(deltaU >= 1){
                 update();
                 updates++;
                 deltaU--;
             }
-
+            
             if(deltaF >= 1){
                 paneljuego.repaint();
                 frames ++;
                 deltaF--;
             }
-
-            if(System.currentTimeMillis() - ultimoCheck >= 1000){
-                ultimoCheck = System.currentTimeMillis();
-                System.out.println("FPS: "+frames + " | UPS: " + updates);
-                frames = 0;
-                updates = 0;
-            }
+            
+            
+        if(System.currentTimeMillis() - ultimoCheck >= 1000){
+        ultimoCheck = System.currentTimeMillis();
+            System.out.println("FPS: "+frames + " | UPS: " + updates);
+            frames = 0;
+            updates = 0;
+        }
         }
     }
-
+    
     public void windowFocusLost(){
         if(Gamestate.state == Gamestate.PLAYING){
             playing.getPlayer().resetDirBooleans();
